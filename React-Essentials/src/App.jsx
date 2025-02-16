30952a538939af1a25ee9cdd07bb92c77f6c79b0
NageshMaddala <nageshvmr@gmail.com>
2025-02-15 20:11:53 -0500

    first component

--- React-Essentials/src/App.jsx
+++ React-Essentials/src/App.jsx
@@ -1,14 +1,29 @@
+function Header() {
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+// All the jsx functions should start with capital letter
+// Jsx functions contain html, javascript, and css code together
+// Jsx functions are called components
+// Components are reusable pieces of code
+// Components can be nested
+// Components are transformed into html by React framework that can be rendered in the browser 
+// React function should always start with a capital letter
+// All built in components are lower case letters, othewise there will be collision
+// To distinguish between built in components and user defined components capital letters are used
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
--- React-Essentials/src/index.jsx
+++ React-Essentials/src/index.jsx
@@ -1,7 +1,19 @@
+// Entry file for the react app
+// It's like a bootstrap file that loads the app
+// This file is responsible for rendering the app
+// and mounting it to the DOM
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
 import "./index.css";
 
 const entryPoint = document.getElementById("root");
-ReactDOM.createRoot(entryPoint).render(<App />);
+// createRoot is responsible for creating a root for the app
+// and mounting it to the DOM
+// App is the main component of the app, the first component to be analyzed and rendered by React
+// And then the children components are rendered
+// Structure of components are rendered
+// React combines all the components into a tree structure and renders them on the dom
+// As a developer, you don't have to worry about the tree structure
+// React does that for you
+ReactDOM.createRoot(entryPoint).render(<App/>);
