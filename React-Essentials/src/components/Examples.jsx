c4eed1bd3156c6706df385fbe7fa7af95a521644
NageshMaddala <nageshvmr@gmail.com>
2025-02-18 22:16:04 -0500

    Wrapper

--- React-Essentials/src/components/Examples.jsx
+++ React-Essentials/src/components/Examples.jsx
@@ -45,6 +45,7 @@ export default function Examples() {
   return (
     <Section title="Examples" id="examples">
       <Tabs
+        buttonsContainer="menu"
         buttons={
           <>
             <TabButton
--- React-Essentials/src/components/Header/Tabs.jsx
+++ React-Essentials/src/components/Header/Tabs.jsx
@@ -1,9 +1,13 @@
 import TabButton from "../TabButton";
 
-export default function Tabs({ children, buttons }) {
+export default function Tabs({ children, buttons, buttonsContainer }) {
+    // buttonsContainer is a prop that is passed to the Tabs component
+    // Below ButtonsContainer is a variable that is assigned the value of buttonsContainer prop
+    // This is done to avoid using the prop directly in the JSX below    
+  const ButtonsContainer = buttonsContainer;
   return (
     <>
-      <menu>{buttons}</menu>
+      <ButtonsContainer>{buttons}</ButtonsContainer>
       {children}
     </>
   );
