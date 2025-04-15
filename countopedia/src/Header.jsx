2b54c86c3199f3f8e25975e5cba88160e74672f6
NageshMaddala <nageshvmr@gmail.com>
2025-04-15 08:13:09 -0400

    Add basic boiler plate code for coutopedia

---
+++ countopedia/src/Header.jsx
@@ -0,0 +1,16 @@
+import logo from "./images/react.png";
+
+function Header() {
+  return (
+    <div className="pt-3 pl-2" style={{borderBottom: "1px solid #777"}} >
+      <img
+        src={logo}
+        alt="logo"
+        style={{ height: "35px", verticalAlign: "top" }}
+      />
+      <span className="h2 pt-4 text-white-50">React Course - Countopedia</span>
+    </div>
+  );
+}
+
+export default Header;
