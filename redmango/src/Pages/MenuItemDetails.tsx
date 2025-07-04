2ec3c02048a828f43a1c07d9ef1146c6700be31a
NageshMaddala <nageshvmr@gmail.com>
2025-07-04 13:24:49 -0400

    MenuItem Details from rtk query

--- redmango/src/Pages/MenuItemDetails.tsx
+++ redmango/src/Pages/MenuItemDetails.tsx
@@ -1,17 +1,51 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useGetMenuItemByIdQuery } from '../Apis/menuItemApi';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 function MenuItemDetails() {
+    // Using Redux Toolkit Query to fetch a single menu item by ID
+    // This will automatically fetch the menu item data based on the ID from the URL
+    const { menuItemId } = useParams();
+    const { data, isLoading } = useGetMenuItemByIdQuery(menuItemId);
+    const navigate = useNavigate();
+
+    const [quantity, setQuantity] = useState(1);
+
+    // We can also do conditional rendering based on the loading state
+    // If the data is still being fetched, we can show a loading message
+    // If the data is fetched, we can display the menu item details
+    // If the data is not found, we can show a message indicating that the item does
+    // not exist or is unavailable
+
+    if (isLoading) {
+        return <div className="d-flex justify-content-center"
+            style={{ width: "100%" }}>Loading...</div>; // Display a loading message while data is being fetched
+    }
+
+    console.log(data);
+
+    const handleQuantity = (counter: number) => {
+        let newQuantity = quantity + counter;
+        if (newQuantity == 0 || newQuantity < 0) {
+            newQuantity = 1; // Prevent quantity from going below 1
+        }
+        setQuantity(newQuantity);
+        return;
+    }
+
     return (
         <div className="container pt-4 pt-md-5">
             <div className="row">
                 <div className="col-7">
-                    <h2 className="text-success">NAME</h2>
+                    <h2 className="text-success">{data.result?.name}</h2>
                     <span>
                         <span
                             className="badge text-bg-dark pt-2"
                             style={{ height: "40px", fontSize: "20px" }}
                         >
-                            CATEGORY
+                            {data.result?.category}
                         </span>
                     </span>
                     <span>
@@ -19,23 +53,23 @@ function MenuItemDetails() {
                             className="badge text-bg-light pt-2"
                             style={{ height: "40px", fontSize: "20px" }}
                         >
-                            SPECIAL TAG
+                            {data.result?.specialTag}
                         </span>
                     </span>
                     <p style={{ fontSize: "20px" }} className="pt-2">
-                        DESCRIPTION
+                        {data.result?.description}
                     </p>
-                    <span className="h3">$10</span> &nbsp;&nbsp;&nbsp;
+                    <span className="h3">{data.result?.price}</span> &nbsp;&nbsp;&nbsp;
                     <span
                         className="pb-2  p-3"
                         style={{ border: "1px solid #333", borderRadius: "30px" }}
                     >
-                        <i
+                        <i onClick={() => { handleQuantity(-1); }}
                             className="bi bi-dash p-1"
                             style={{ fontSize: "25px", cursor: "pointer" }}
                         ></i>
-                        <span className="h3 mt-3 px-3">XX</span>
-                        <i
+                        <span className="h3 mt-3 px-3">{quantity}</span>
+                        <i onClick={() => { handleQuantity(+1); }}
                             className="bi bi-plus p-1"
                             style={{ fontSize: "25px", cursor: "pointer" }}
                         ></i>
@@ -48,7 +82,8 @@ function MenuItemDetails() {
                         </div>
 
                         <div className="col-5 ">
-                            <button className="btn btn-secondary form-control">
+                            <button className="btn btn-secondary form-control"
+                                onClick={() => navigate(-1)}>
                                 Back to Home
                             </button>
                         </div>
@@ -56,7 +91,7 @@ function MenuItemDetails() {
                 </div>
                 <div className="col-5">
                     <img
-                        src="https://via.placeholder.com/150"
+                        src={data.result?.image}
                         width="100%"
                         style={{ borderRadius: "50%" }}
                         alt="No content"
