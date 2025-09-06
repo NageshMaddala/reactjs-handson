0d4fbedfe403ca58da6903453e0d2d641824f049
NageshMaddala <nageshvmr@gmail.com>
2025-09-06 17:33:56 -0400

    Added migrations and code related to MenuItem and retrievieng blob

---
+++ RedMango_API/RedMango_API/Controllers/MenuItemController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedMango_API.Data;
+using RedMango_API.Models;
+
+namespace RedMango_API.Controllers
+{
+    [Route("api/MenuItem")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        private ApiResponse _response;
+
+        public MenuItemController(ApplicationDbContext db)
+        {
+            _db = db;
+            _response = new ApiResponse();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMenuItems()
+        {
+            _response.Result = _db.MenuItems;
+            _response.StatusCode = System.Net.HttpStatusCode.OK;
+            return Ok(_response);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetMenuItem(int id)
+        {
+            if (id == 0)
+            {
+                _response.StatusCode = System.Net.HttpStatusCode.BadRequest;
+                return BadRequest(_response);
+            }
+
+            MenuItem menuItem = _db.MenuItems.FirstOrDefault(u => u.Id == id);
+
+            if (menuItem == null)
+            {
+                _response.StatusCode = System.Net.HttpStatusCode.NotFound;
+                return NotFound(_response);
+            }
+
+            _response.Result = menuItem;
+            _response.StatusCode = System.Net.HttpStatusCode.OK;
+            return Ok(_response);
+        }
+    }
+}
--- RedMango_API/RedMango_API/Program.cs
+++ RedMango_API/RedMango_API/Program.cs
@@ -1,7 +1,9 @@
+using Azure.Storage.Blobs;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using RedMango_API.Data;
 using RedMango_API.Models;
+using RedMango_API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,9 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultDbConnection"));
 });
+
+builder.Services.AddSingleton(u => new BlobServiceClient(builder.Configuration.GetConnectionString("StorageAccount")));
+builder.Services.AddSingleton<IBlobService, BlobService>();
 
 // This will create all the identity tables from microsoft
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
