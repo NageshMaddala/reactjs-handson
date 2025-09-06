b46c947fa6b8e5d5678543b8475bbaf3024bdad3
NageshMaddala <nageshvmr@gmail.com>
2025-09-06 10:22:15 -0400

    Added boilerplate code for api

---
+++ RedMango_API/RedMango_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RedMango_API.Models;
+
+namespace RedMango_API.Data
+{
+    // .Net Identity will do the heavy lifting for us
+    // IdentityDbContext will provide registration, user role table and other related tables
+    // They are already configured by microsoft for us
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
